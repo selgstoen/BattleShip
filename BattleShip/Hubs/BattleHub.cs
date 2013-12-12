70521aed1648e54e5ea4cd1585720f77cc928016
Svein Elgstøen <svein.elgstoen@statkraft.com>
2013-12-12 07:15:57 +0100

    Initial check in

---
+++ BattleShip/Hubs/BattleHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace BattleShip.Hubs
+{
+    public class BattleHub : Hub
+    {
+        public void Send(string name, string row, string column)
+        {
+            Clients.All.addNewMessageToPage(name, row, column);
+        }
+    }
+}
---
+++ BattleShip/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BattleShip.Startup))]
+namespace BattleShip
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
